0f4464ff21da287b6dc53c66c26b217c653537a5
abdo <abdo.mohamadeen@gmail.com>
2022-02-07 15:33:54 +0400

    Enhance written game rules and readme file

--- src/pages/Main/hooks/useGameIntro.js
+++ src/pages/Main/hooks/useGameIntro.js
@@ -103,20 +103,22 @@ const useGameIntro = () => {
       <Box w='45rem' wt='30rem'>
         <Text type='h3'>Welcome to High-Low! ⬆️ ⬇️ </Text>
         <Text type='h4'>The coolest cards game ever. 🥳</Text>
-        <Text>
-          <Span fw='bold'>Here are the rules:</Span> <br />
-          - The game starts with a deck of cards, the first card is drawn from
-          that deck and added to a pile. <br />- You then have to guess whether
-          the next card's number will be a higher number or lower number than
-          the latest card. If you are correct, we add that card to the pile. If
-          you are incorrect, you get a point for every card that was in the pile
-          at that time (for example, if 10 cards were in the pile, you would get
-          10 points).And the pile is cleared. <br />
-          - After users have 3 successful guesses in a row, they can "pass" to
-          the other player. By pass we mean that if you start as Player 1, you
-          can change to Player 2. <br /> - Player 2 can pass back to Player 1
-          once they get 3 successful guesses in a row. The player with the least
-          number of points at the end wins.
+        <Text type='h5'>
+          <Span fw='bold'>
+            - You try to guess whether the next drawn card will have a higher
+            value or a lower value that the current card!
+          </Span>{' '}
+          <br /> <br />
+          - If you are correct, we add that card to the pile. If you are
+          incorrect, you get a point for every card that was in the pile at that
+          time (for example, if 10 cards were in the pile, you would get 10
+          points).And the pile is cleared. <br /> <br />- After you get 3
+          successful guesses in a row, you "pass" to the other player. <br />
+          <br />
+          <Span fw='bold'>
+            - The player with the least number of points at the of the game
+            wins!
+          </Span>
         </Text>
         <Button
           bgc={theme.colors.mainLight}
